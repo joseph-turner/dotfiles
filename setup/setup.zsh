f96b26dff75ef5ca103380c8ef195fed3b9bcb12
Joseph Turner <jturner@overstock.com>
2022-01-25 21:22:31 -0600

    fixes thing

--- setup/setup.zsh
+++ setup/setup.zsh
@@ -103,15 +103,15 @@ symlink_files() {
     sourceFile="$DOTFILES_DIR/$i"
     targetFile="$HOME/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
 
-    if [ ! -e "$targetFile" ]; then
-      execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+    if [[ ! -e $targetFile ]]; then
+      ln -fs $sourceFile $targetFile
     elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
       print_success "$targetFile → $sourceFile"
     else
       ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
       if answer_is_yes; then
         rm -rf "$targetFile"
-        execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        ln -fs $sourceFile $targetFile
       else
         print_error "$targetFile → $sourceFile"
       fi
