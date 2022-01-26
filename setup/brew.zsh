54e7540e6bae5a6f8e172e95958353ae47dfb6ba
Joseph Turner <jturner@overstock.com>
2022-01-25 21:54:25 -0600

    sudo fix

--- setup/brew.zsh
+++ setup/brew.zsh
@@ -3,7 +3,7 @@
 # Installs Homebrew and some of the common dependencies needed/desired for software development
 
 # Ask for the administrator password upfront
-# sudo -v
+sudo -v
 
 # Check for Homebrew and install it if missing
 if ! type brew &> /dev/null; then
