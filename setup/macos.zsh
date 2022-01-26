e50d62977d055d7b72250d14ba09f9190894afcc
Joseph Turner <jturner@overstock.com>
2022-01-26 15:45:14 -0600

    removed unused macos preferences

--- setup/macos.zsh
+++ setup/macos.zsh
@@ -130,42 +130,6 @@ defaults write com.apple.dock ResetLaunchPad -bool true
 defaults write com.apple.dock wvous-tl-corner -int 5
 
 ###############################################################################
-# Spotlight                                                                   #
-###############################################################################
-
-# Change indexing order and disable some search results
-defaults write com.apple.spotlight orderedItems -array \
-  '{"enabled" = 1;"name" = "APPLICATIONS";}' \
-  '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
-  '{"enabled" = 1;"name" = "DIRECTORIES";}' \
-  '{"enabled" = 1;"name" = "MENU_EXPRESSION";}' \
-  '{"enabled" = 1;"name" = "MENU_DEFINITION";}' \
-  '{"enabled" = 1;"name" = "MENU_CONVERSION";}' \
-  '{"enabled" = 1;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}' \
-  '{"enabled" = 1;"name" = "SOURCE";}' \
-  '{"enabled" = 0;"name" = "PDF";}' \
-  '{"enabled" = 0;"name" = "FONTS";}' \
-  '{"enabled" = 0;"name" = "DOCUMENTS";}' \
-  '{"enabled" = 0;"name" = "MESSAGES";}' \
-  '{"enabled" = 0;"name" = "CONTACT";}' \
-  '{"enabled" = 0;"name" = "EVENT_TODO";}' \
-  '{"enabled" = 0;"name" = "IMAGES";}' \
-  '{"enabled" = 0;"name" = "BOOKMARKS";}' \
-  '{"enabled" = 0;"name" = "MUSIC";}' \
-  '{"enabled" = 0;"name" = "MOVIES";}' \
-  '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-  '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-  '{"enabled" = 0;"name" = "MENU_OTHER";}' \
-  '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
-
-# Load new settings before rebuilding the index
-killall mds &> /dev/null
-# Make sure indexing is enabled for the main volume
-sudo mdutil -i on / &> /dev/null
-# Rebuild the index from scratch
-sudo mdutil -E / &> /dev/null
-
-###############################################################################
 # Terminal & iTerm 2                                                          #
 ###############################################################################
 
